bcea6bff0be9cf31ecb91b5535c066f3c411f25e
Vivek <vivek.p@rxprism.com>
2020-09-11 19:15:31 +0530

    label added

--- Test19thproject/ViewController.swift
+++ Test19thproject/ViewController.swift
@@ -21,6 +21,14 @@ class ViewController: UIViewController {
         
         self.view .addSubview(button)
         
+        // Adding label
+        
+     let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+        label.center = CGPoint(x: 160, y: 285)
+        label.textAlignment = .center
+        label.text = "I'm a test label"
+        self.view.addSubview(label)
+        
         
     }
 
